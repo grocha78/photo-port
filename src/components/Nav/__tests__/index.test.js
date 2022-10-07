94d572b0a87bba37132e841508458a87f72d7492
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-10-07 13:02:53 -0500

    gallery

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,16 +3,28 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
     // baseline test
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory} />);
     });
     // snapshot test
     it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
+        const { asFragment } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory} />);
 
         expect(asFragment()).toMatchSnapshot();
     });
@@ -20,19 +32,25 @@ describe('Nav component', () => {
 
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
-        const { getByLabelText } = render(<Nav />);
+        const { getByLabelText } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory} />);
 
-        // eslint-disable-next-line testing-library/prefer-screen-queries
+        
         expect(getByLabelText('camera')).toHaveTextContent('📸');
     });
 
 describe('links are visible', () => {
     it('inserts text into the links', () => {
-        const { getByTestId } = render(<Nav />);
+        const { getByTestId } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory} />);
 
-        // eslint-disable-next-line testing-library/prefer-screen-queries
+       
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-        // eslint-disable-next-line testing-library/prefer-screen-queries
+        
         expect(getByTestId('about')).toHaveTextContent('About me');
     });
 })
